1ff18fb85e0d3bbdd2dfb86e14997ec7edf8af67
mutobui <mutobui@gmail.com>
2022-05-05 20:33:59 +0700

    remove target

--- hello_cargo/restaurant/target/rls/debug/deps/restaurant-221ddd0aa75ddbb6.d
+++
@@ -1,7 +0,0 @@
-/Users/admin/Projects/Rust/The-Rust-Programming-Language/hello_cargo/restaurant/target/rls/debug/deps/restaurant-221ddd0aa75ddbb6.rmeta: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-/Users/admin/Projects/Rust/The-Rust-Programming-Language/hello_cargo/restaurant/target/rls/debug/deps/restaurant-221ddd0aa75ddbb6.d: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-src/lib.rs:
-src/front_of_house.rs:
-src/front_of_house/hosting.rs:
--- hello_cargo/restaurant/target/rls/debug/deps/restaurant-70674ef8e41ed4e3.d
+++
@@ -1,7 +0,0 @@
-/Users/admin/Projects/Rust/The-Rust-Programming-Language/hello_cargo/restaurant/target/rls/debug/deps/restaurant-70674ef8e41ed4e3.rmeta: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-/Users/admin/Projects/Rust/The-Rust-Programming-Language/hello_cargo/restaurant/target/rls/debug/deps/restaurant-70674ef8e41ed4e3.d: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-src/lib.rs:
-src/front_of_house.rs:
-src/front_of_house/hosting.rs:
